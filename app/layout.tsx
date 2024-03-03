36b2b98282789e4d965112f1ae764627d7cc55d4
Mykael Alexandre <mykaelalexandre20@gmail.com>
2024-03-03 13:03:39 -0300

    feat: add tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,52 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+/* eslint-disable @next/next/next-script-for-ga */
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import { ToasterProvider } from '@/components/toaster-provider'
-import { ModalProvider } from '@/components/modal-provider'
-import { CrispProvider } from '@/components/crisp-provider'
+import { ToasterProvider } from "@/components/toaster-provider";
+import { ModalProvider } from "@/components/modal-provider";
+import { CrispProvider } from "@/components/crisp-provider";
 
-import './globals.css'
+import "./globals.css";
 
-const font = Inter({ subsets: ['latin'] });
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
-  description: 'AI Platform',
-}
+  title: "Genius",
+  description: "AI Platform",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K6N65HSL')`,
+            }}
+          />
+        </head>
         <CrispProvider />
         <body className={font.className}>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K6N65HSL"
+            height="0"
+            width="0"
+          ></iframe>
           <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
